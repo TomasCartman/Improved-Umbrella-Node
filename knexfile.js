ac93ade9f50245fa93763bd0a150353787a821cf
Tomas Cartman <marco12y@outlook.com>
2020-04-17 02:02:21 -0300

    change knexfile to heroku database

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,10 @@
 module.exports = {
     client: 'postgresql',
     connection: {
-      database: 'improved_umbrella',
-      user:     'postgres',
-      password: 'root'
+      host: 'ec2-52-71-85-210.compute-1.amazonaws.com',
+      database: 'd68d6vds8i89bm',
+      user:     'vqxjwyxkkdpffb',
+      password: '758a9a8c9a03b91b5627a8c680f0351b371f419d96231812cfc2c9f2bdd211a5'
     },
     pool: {
       min: 2,
